496f70a35f149534cdd6333eab0a136edbbf1896
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2023-07-15 03:13:22 +0530

    fetching products price in real time

--- shop/static/shop/js/cart.js
+++ shop/static/shop/js/cart.js
@@ -1,14 +1,34 @@
 // Logic for fixing the price contianer at the top
-window.addEventListener("scroll", function () {
-  let cartContainer = document.querySelector(".cart-container");
+window.addEventListener('scroll', (e) => {
   let priceContainer = document.querySelector(".cart-price-container");
-  let scrollTop = cartContainer.scrollTop || document.documentElement.scrollTop;
-  if (scrollTop > 0) {
-    priceContainer.style.top = scrollTop + "px";
-  } else {
-    priceContainer.style.top = 0 + "px";
+  if(window.scrollY >= 0){
+    priceContainer.style.top = window.scrollY + "px";
   }
 });
+
+// ADDING THE LOGIC INTO ITEM QUANTITY COUNTER
+let addBtn = document.querySelectorAll(".operation-add");
+let subBtn = document.querySelectorAll(".operation-subtract");
+let displayQuantity = document.querySelectorAll(".quantity");
+let allProductCards = document.querySelectorAll(".cart-product-det");
+let isRemovedBtn = false;
+
+if (localStorage.getItem("Quantity") == null) {
+  var cart = {};
+  displayQuantity.forEach((item) => {
+    item.innerHTML = 1;
+  });
+  fetchProductsInStorage(); // When cart is empty and then the product is added
+} else {
+  cart = getCart();
+  allProductCards.forEach((item) => {
+    let prodId = item.id;
+    if (!cart.hasOwnProperty(prodId)) {
+      cart[prodId] = 1;
+      updateCart(cart);
+    }
+  });
+}
 
 // Removing the item by sending the product id to the url
 import { sendingProductId } from "./module.js";
@@ -18,11 +38,15 @@ function removeCartItem(item) {
     let prodId = e.target.id;
     let url = "/shop/" + prodId + "/rem/";
     sendingProductId(url);
+    isRemovedBtn = true;
+    if (isRemovedBtn) {
+      fetchPrice(cart);
+    }
     item.parentElement.parentElement.parentElement.remove();
     localStorage.removeItem("product" + prodId);
     removeItem(prodId);
     let keyValue = JSON.parse(localStorage.getItem("Quantity"));
-    if (Object.keys(keyValue).length === 0){
+    if (Object.keys(keyValue).length === 0) {
       localStorage.removeItem("Quantity");
     }
   });
@@ -31,32 +55,12 @@ removeBtn.forEach(function (item) {
   removeCartItem(item);
 });
 
-// ADDING THE LOGIC INTO ITEM QUANTITY COUNTER
-let addBtn = document.querySelectorAll(".operation-add");
-let subBtn = document.querySelectorAll(".operation-subtract");
-let displayQuantity = document.querySelectorAll(".quantity");
-let allProductCards = document.querySelectorAll(".cart-product-det");
-
-if (localStorage.getItem("Quantity") == null) {
-  var cart = {};
-  displayQuantity.forEach((item) => {
-    item.innerHTML = 1;
-  });
-  fetchProductsInStorage();
-} else {
-  console.log(cart);
-  cart = getCart();
-}
-
 function fetchProductsInStorage() {
-  console.log("in the fucntion");
   allProductCards.forEach((item) => {
+    let prodId = item.id;
     if (item) {
-      let prodId = item.id;
       cart[prodId] = 1;
-      let cartObjToStr = JSON.stringify(cart);
-      localStorage.setItem("Quantity", cartObjToStr);
-      console.log("Chal rha h");
+      updateCart(cart);
     }
   });
 }
@@ -82,6 +86,7 @@ function addItem(cart, product) {
     cart["product " + product] = 2;
   }
   updateCart(cart);
+  fetchPrice(cart);
 }
 
 function subtractItem(cart, product) {
@@ -94,6 +99,7 @@ function subtractItem(cart, product) {
     console.log("There is no product");
   }
   updateCart(cart);
+  fetchPrice(cart);
 }
 
 function updateCart(cart) {
@@ -121,3 +127,42 @@ function removeItem(product) {
   delete cartData["product " + product];
   updateCart(cartData);
 }
+
+// REALTIME PRICE FETCH AND SHOWING ON THE CARD
+function fetchPrice(cart) {
+  let ogPriceSum = 0;
+  let discountValue = 0;
+  let discountPriceSum = 0;
+  let totalPrice = 0;
+  let itemCount = 0;
+  let showOgPrice = document.getElementById("og-price");
+  let showDiscountPrice = document.getElementById("discount-price");
+  let showTotalAmount = document.getElementById("total-amount");
+  let showItemCount = document.getElementById("item-count");
+  let keysOfCart = Object.keys(cart);
+  itemCount = keysOfCart.length;
+
+  for (let key in cart) {
+    if (cart.hasOwnProperty(key)) {
+      let value = cart[key];
+      let ogPrice;
+      let discountPrice;
+      let prodId = key.slice(8);
+      let ogPriceElement = document.getElementById(`og-${prodId}`);
+      let discountPriceElement = document.getElementById(`discount-${prodId}`);
+      ogPrice = parseInt(ogPriceElement.innerHTML);
+      discountPrice = parseInt(discountPriceElement.innerHTML);
+      ogPriceSum += ogPrice * value;
+      discountPriceSum += discountPrice * value;
+      discountValue = ogPriceSum - discountPriceSum;
+      totalPrice = ogPriceSum - discountValue;
+    }
+  }
+  showOgPrice.innerHTML = ogPriceSum;
+  showDiscountPrice.innerHTML = "-" + discountValue;
+  showTotalAmount.innerHTML = totalPrice;
+  showItemCount.innerHTML = `Price (${itemCount} item)`;
+
+  console.log("Success");
+}
+fetchPrice(cart);
